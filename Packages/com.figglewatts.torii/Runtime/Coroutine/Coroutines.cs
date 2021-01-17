27f6d8ebfc9df9c18a69dde6c0152b18833686b4
Figglewatts <figglewatts@hotmail.co.uk>
2021-01-17 15:08:24 +0000

    Add utility methods to coroutines

--- Packages/com.figglewatts.torii/Runtime/Coroutine/Coroutines.cs
+++ Packages/com.figglewatts.torii/Runtime/Coroutine/Coroutines.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections;
 using Torii.Util;
 
 namespace Torii.Coroutine
 {
     /// <summary>
     /// Used to allow for access to StartCoroutine anywhere, like within a ScriptableObject.
-    /// Just do Coroutines.Instance.StartCoroutine(...)
+    /// Just do Coroutines.Begin(...)
     /// </summary>
     public class Coroutines : MonoSingleton<Coroutines>
     {
+        protected static IEnumerator callNextFrame(Action callback)
+        {
+            yield return null;
+            callback();
+        }
+
+        public static UnityEngine.Coroutine Begin(IEnumerator coroutine) { return Instance.StartCoroutine(coroutine); }
+
+        public static void CallNextFrame(Action callback) { Begin(callNextFrame(callback)); }
     }
 }
