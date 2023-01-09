fca7fa05e241c2d997de798712e893e6a3fe9a4a
Figglewatts <me@figglewatts.co.uk>
2023-01-09 20:53:56 +0000

    TimeSince will now work in editor

--- Packages/com.figglewatts.torii/Runtime/Util/TimeSince.cs
+++ Packages/com.figglewatts.torii/Runtime/Util/TimeSince.cs
@@ -1,5 +1,8 @@
 using System.Globalization;
-using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Torii.Util
 {
@@ -8,10 +11,24 @@ namespace Torii.Util
     {
         private float time;
 
-        public static implicit operator float(TimeSince ts) { return Time.time - ts.time; }
+        public static implicit operator float(TimeSince ts) { return getTime() - ts.time; }
 
-        public static implicit operator TimeSince(float ts) { return new TimeSince {time = Time.time - ts}; }
+        public static implicit operator TimeSince(float ts) { return new TimeSince {time = getTime() - ts}; }
 
         public override string ToString() { return ((float)this).ToString(CultureInfo.InvariantCulture); }
+
+        public void Reset()
+        {
+            time = getTime();
+        }
+
+        private static float getTime()
+        {
+#if UNITY_EDITOR
+            return (float)EditorApplication.timeSinceStartup;
+#else
+            return Time.time;
+#endif
+        }
     }
 }
